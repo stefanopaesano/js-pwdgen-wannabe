747435f13f8cda0a3a71b38c8fe5b144d7547b5c
stefano paesano <stefanopaesano022@gmail.com>
2023-11-30 17:28:50 +0100

    esercizio js

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+
+var nome = prompt("Inserisci il tuo nome:");
+
+
+var cognome = prompt("Inserisci il tuo cognome:");
+
+
+var colorePreferito = prompt("Inserisci il tuo colore preferito:");
+
+
+// Crea una stringa con i dati dell'utente
+var risultato = nome + cognome + colorePreferito + "114";
+
+// Mostra i dati sulla pagina
+document.write("<h1>" + risultato + "</h1>");
